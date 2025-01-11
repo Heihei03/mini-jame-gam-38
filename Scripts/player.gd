084728705c2471a74e3cfbeb2e7389dafd758bc2
Heihei03 <22041201+Heihei03@users.noreply.github.com>
2025-01-11 14:53:11 +1300

    Added tileset

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed : float = 300.0
+
+var direction : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +11,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	direction = Input.get_axis("left", "right")
+	velocity.x = direction * speed
 	pass
