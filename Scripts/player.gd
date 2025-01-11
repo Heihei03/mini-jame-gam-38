a51517ae4c659daa97750eca7d9678d56bed1725
Heihei03 <22041201+Heihei03@users.noreply.github.com>
2025-01-11 15:59:44 +1300

    Add sprite and gravity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed : float = 300.0
 
 var direction : float = 0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	
 	direction = Input.get_axis("left", "right")
 	velocity.x = direction * speed
+	
+	move_and_slide()
 	pass
