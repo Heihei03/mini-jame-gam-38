17a45f146f177794bfa350d4ba83b1e828c2098c
Heihei03 <22041201+Heihei03@users.noreply.github.com>
2025-01-12 21:24:47 +1300

    Add crown

---
+++ Scripts/crown.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+@onready var player = get_parent().get_node("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body == player:
+		self.hide()
