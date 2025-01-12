cce72dd5778d36edba988b0575f231b6435ebe18
Kade1O1 <forsytkade@myvuw.ac.nz>
2025-01-13 00:58:40 +1300

    implemented stealth hat

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -37,12 +37,15 @@ func _physics_process(delta):
 # Sight functions
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if body == _player :
+		if _player.hat_id == 1 :
+			timer.wait_time = 3
+		else :
+			timer.wait_time = 1
 		timer.start()
 
 func _on_area_2d_2_body_exited(body: Node2D) -> void:
 	if body == _player :
 		timer.stop()
-		timer.wait_time = 1
 
 func _on_timer_2_timeout() -> void:
 	if not hostile :
